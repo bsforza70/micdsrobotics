13e12c1159bd7ec24e8ba5df09d628d086c4f982
AsianPsychoBoy <ziyang_99@outlook.com>
2015-12-24 15:17:33 -0600

    make changes to the array to add goals

---
+++ goals.php
@@ -0,0 +1,88 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>MICDS Robotics</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="Bob Sforza & Charlie Biggs" />
+<meta name="author" content="MICDS Robotics" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+
+<link href="skins/default.css" rel="stylesheet" />
+
+</head>
+<body>
+<div id="wrapper">
+	<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><span>MICDS</span> Robotics</a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <!--<li class="dropdown">
+                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false">Features <b class=" icon-angle-down"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="typography.html">Typugraphy</a></li>
+                                <li><a href="components.html">Components</a></li>
+								<li><a href="pricingbox.html">Pricing box</a></li>
+                            </ul>
+                        </li>-->
+                        <!--<li><a href="portfolio.html">Portfolio</a></li>-->
+                        <li><a href="blog.html">Blog</a></li>
+                        <!--<li><a href="contact.html">Contact</a></li>-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header>
+    
+    <section id='planner_table'>
+        <h1 id="inner-headline">Goals</h1>
+        
+        <?php 
+        $events = array (
+            array('Notebook'),
+            array('Construction'),
+            array('Programming'),
+            array('Outreach'),
+            array('3D Printing'),
+        );
+        
+        echo '<table>';
+        for (i=0, i<=5, i++) {
+            echo '<tr>';
+            foreach ($events as $event){
+                echo '<td>'.$events[$i].'</td>'
+            }
+            echo '</tr>'
+        }
+        echo '</table>';
+        
+        
+        <form method="post" action="goals-processing.php">
+            <label for="planner_input">what are you going to add?</label>
+            <input type="text" id="planner_input" name="input">
+            <input type="submit" value="submit"><br/>
+            <input type="radio" value="-1" name="event_type" checked>Please select an event type
+            <input type="radio" value="0" name="event_type">Notebook
+            <input type="radio" value="1" name="event_type">Construction
+            <input type="radio" value="2" name="event_type">Programming
+            <input type="radio" value="3" name="event_type">Outreach
+            <input type="radio" value="4" name="event_type">3D Printing
+        </form>
+        
+    </section>
