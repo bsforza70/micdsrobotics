2230d187b80d795b57d8ec6226c00a0757cd6def
AsianPsychoBoy <ziyang_99@outlook.com>
2015-12-26 06:54:02 -0600

    v2
    v1 is utterly the worst thing ever...
    v2 is much better and stable, except that i haven't styled it yet.

--- goals.php
+++ goals.php
@@ -56,6 +56,8 @@
         
         <?php 
 
+        var_dump($_POST);
+
         try {
             $events_db = new PDO("mysql:host=localhost;dbname=micdsrobotics", "root");
             $events_db -> setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -65,126 +67,138 @@
             exit;
         }
 
-
-        try {
-            $results2 = $events_db -> prepare("SELECT Notebook, Construction, Programming, Outreach, 3D_Printing FROM events");
-            $results2 -> execute();
-        } catch (Exception $error) {
-            echo "could not retrieve events data, details below:"."</br>".$error;
-            exit;
-        }
-
-        $test1 = $results2->fetchAll(PDO::FETCH_ASSOC);
-        $switch = True;
-        foreach ($test1 as $test_row) {
-            foreach ($test_row as $event_type => $event) {
-                if ($event_type === $_POST["event_type"] and $event === $_POST["input"]) {
-                    echo '<pre>';
-                    echo 'You have entered repeated data at ' . $event_type . ', content: ' . $event;
-                    echo '</pre>';
-                    $switch = False;
-                }
-            }
-        }
-        $test1 = null;
-
-        try {
-            $results1 = $events_db -> query("SELECT * FROM events ORDER BY id DESC LIMIT 1");
-        } catch (Exception $error) {
-            echo "could not retrieve input data, details below:"."</br>".$error;
-            exit;
-        }
-
-        $test2 = $results1->fetch(PDO::FETCH_ASSOC);
-        foreach ($test2 as $event_type => $event) {
-            if ($event_type === $_POST["event_type"] and isset($event)) {
-                echo '<pre>';
-                echo 'You have attempted to enter more than one goals at ' . $event_type . ', content: ' . $event;
-                echo '</pre>';
-                $switch = False;
-            }
-        }
-
-        if ($_POST["event_type"] != -1 and !empty(trim($_POST["input"])) and $switch) {
+        if (isset($_POST["event_type"]) and isset($_POST["input"])) {
             try {
-                $results1 = $events_db -> query("SELECT * FROM events ORDER BY id DESC LIMIT 1");
+                $results2 = $events_db -> prepare("SELECT Notebook, Construction, Programming, Outreach, 3D_Printing FROM events");
+                $results2 -> execute();
             } catch (Exception $error) {
-                echo "could not retrieve input data, details below:"."</br>".$error;
+                echo "could not retrieve events data, details below:"."</br>".$error;
                 exit;
             }
 
-            $content = $results1->fetch(PDO::FETCH_BOTH); //Select & Get the most recent row. 
-            $current_id = $content[0];
-            $new_id = $current_id + 1;
+            $test1 = $results2->fetchAll(PDO::FETCH_ASSOC);
+            $switch = True;
+            foreach ($test1 as $test_row) {
+                foreach ($test_row as $event_type => $event) {
+                    if ($event_type === $_POST["event_type"] and $event === $_POST["input"]) {
+                        echo '<pre>';
+                        echo 'You have entered repeated data at ' . $event_type . ', content: ' . $event;
+                        echo '</pre>';
+                        $switch = False;
+                    }
+                }
+            }
+            $test1 = null;
 
-            $sql1 = "INSERT INTO events (" . $_POST["event_type"] . ", id) VALUES ('" . $_POST["input"] . "', '" . $new_id . "');";//Execute this if the most recent colum is filled. (Update contents inside the most recent row.)
-            $sql2 = "UPDATE events SET " . $_POST["event_type"] . "= '" . $_POST["input"] . "' WHERE id = " . $current_id . ";";//Execute this if the most recent colum contains null values. (Insert a new row and that row would become the most recent one.)
-            $empty_row = False;                                                                                            #/#
-                                                                                                                          #/#
-            for ($i=0; $i<=5; $i++) {                                                                                    #/#
-                $empty_row = ($empty_row OR is_null($content[$i]));                                                     #/#
-            }//Checks to see if all the colums in the most recent row is populated.                  #                 #/#
-                                                                                                  #/#                 #/#
-            if (!$empty_row) {                                                                  #/#                  #/#
-                try {                                                                         #/#                   #/#
-                    $events_db -> exec($sql1);                                              #/#                    #/#
-                } catch (Exception $error) {                                              #/#                     #/#
-                    echo "could not retrieve input data, details below:"."</br>".$error;############################
-                    exit;                                                                 #\#
-                }                                                                           #\#
-            } else {                                                                          #\#
-                try {                                                                           #\#
-                    $events_db -> exec($sql2);                                                    #\#
-                } catch (Exception $error){                                                          #
-                    echo "could not retrieve input data, details below:"."</br>".$error;
+            if (!empty(trim($_POST["input"])) and $switch) {
+
+                $latest_id = $_POST["row_id"];
+                $new_id = $latest_id + 1; 
+                $sql2 = "UPDATE events SET " . $_POST["event_type"] . "= '" . $_POST["input"] . "' WHERE id = " . $latest_id . ";";//Updating the values
+
+                try {
+                    $events_db -> exec($sql2); 
+                } catch (Exception $error){ 
+                    echo "could not create goal, details below:"."</br>".$error;
                     exit;
                 }
             }
         }//Detects imput for goals events and update the database. 
 
-        //if ($_POST["delete"] == delete){
+        if (isset($_POST["row_request"])) {
 
-        //}//Detects deletion requests and update the database. 
+            $latest_id = $_POST["row_id"];
+            $new_id = $latest_id + 1; 
+            $sql1 = "INSERT INTO events (id) VALUES ('" . $new_id . "');";//Execute to add one row with only an id colum to the database if the add row button is pressed. 
+            $sql3 = "DELETE FROM events WHERE id =" . $latest_id .  ";";
+            $sql5 = "UPDATE events SET id=id+1 WHERE id >= " . $new_id . ";";
+            $sql6 = "UPDATE events SET id=id-1 WHERE id >= " . $new_id . ";";
+
+            if ($_POST["row_request"]=='+') {
+                try { 
+                    $events_db -> exec($sql5.$sql1); 
+                } catch (Exception $error) {
+                    echo "could not create row, details below:"."</br>".$error;
+                    exit;
+                } 
+            } elseif ($_POST["row_request"]=='-') {
+                try {
+                    $events_db -> exec($sql6.$sql3); 
+                } catch (Exception $error){ 
+                    echo "could not delete row, details below:"."</br>".$error;
+                    exit;
+                }
+            }//Add rows to below or delete rows
+        }
+
+        if (isset($_POST["delete"])) {
+            if ($_POST["delete"] == delete){
+                $latest_id = $_POST["row_id"];
+                $sql4 = "UPDATE events SET " . $_POST["event_type"] . "=null WHERE id = " . $latest_id . ";";//removing data
+                try {
+                        $events_db -> exec($sql4); 
+                    } catch (Exception $error){ 
+                        echo "could not delete goal, details below:"."</br>".$error;
+                        exit;
+                    }
+            }//Detects deletion requests and update the database. 
+        }
 
         try {
-            $results2 = $events_db -> prepare("SELECT Notebook, Construction, Programming, Outreach, 3D_Printing FROM events");
+            $results2 = $events_db -> prepare("SELECT Notebook, Construction, Programming, Outreach, 3D_Printing FROM events ORDER BY id ASC");
             $results2 -> execute();
         } catch (Exception $error) {
             echo "could not retrieve events data, details below:"."</br>".$error;
             exit;
         }
         
-        $content = $results2->fetchAll(PDO::FETCH_NUM);
+        $content = $results2->fetchAll(PDO::FETCH_ASSOC);
 
         //Now the content variable is a two dimentional array that stores all the events. 
         $event_db=null;
         //displaying the content
         echo '<div class="content">';
         foreach ($content as $row_id => $events_array) {
+            var_dump($row_id);
             echo '<div class="row">';
             echo '<div class=col-lg-1></div>';
-            foreach ($events_array as $event) {
+            foreach ($events_array as $event_type => $event) {
                 echo '<div class=col-lg-2><p>'.$event.'</p>';
                 if (!is_null($event) and ($row_id != 0)) {
                     echo '<form method="post", action="goals.php">
-                             <input type="button" value="delete" name="delete">
+                            <input type="hidden" value="delete" name="delete">
+                            <input type="hidden" value="' . $row_id . '" name="row_id">
+                            <input type="hidden" value="' . $event_type . '" name="event_type">
+                            <input type="hidden" value="' . date(DATE_RSS) . '" name="time">
+                            <input type="submit" value="delete">
                          </form>';//delete button
+                }
+                if (is_null($event) and ($row_id != 0)) {
+                    echo '<form method="post", action="goals.php">
+                            <label for="planner_input">Add:</label>
+                            <input type="textarea" id="planner_input" name="input" autocomplete="on">
+                            <input type="hidden" value="' . $row_id . '" name="row_id">
+                            <input type="hidden" value="' . $event_type . '" name="event_type">
+                            <input type="hidden" value="' . date(DATE_RSS) . '" name="time">
+                            <input type="submit" value="submit">
+                         </form>';//add button
                 }
                 echo '</div>';
             }
             echo '</div>';
+            echo '<form method="post", action="goals.php">
+                    <input type="hidden" value="+" name="row_request">
+                    <input type="hidden" value="' . $row_id . '" name="row_id">
+                    <input type="submit" value="+">
+                </form>';
+            if ($row_id != 0) {
+                echo '<form method="post", action="goals.php">
+                    <input type="hidden" value="-" name="row_request">
+                    <input type="hidden" value="' . $row_id . '" name="row_id">
+                    <input type="submit" value="-">
+                </form>';
+            }
         }
         ?>
-        <form method="post" action="goals.php">
-            <label for="planner_input">What are you going to add?</label>
-            <input type="textarea" id="planner_input" name="input" autocomplete="on"></br>
-            <input type="radio" value="-1" name="event_type" checked>Please select an event type:
-            <input type="radio" value="Notebook" name="event_type">Notebook
-            <input type="radio" value="Construction" name="event_type">Construction
-            <input type="radio" value="Programming" name="event_type">Programming
-            <input type="radio" value="Outreach" name="event_type">Outreach
-            <input type="radio" value="3D_Printing" name="event_type">3D Printing
-            <input type="submit" value="submit">
-        </form>
         
     </section>
